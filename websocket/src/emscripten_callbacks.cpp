e6ac25dd22807e7466d0d4d290d6e3c91d184a65
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-02-18 11:38:43 +0100

    No need to check if the connection is valid

--- websocket/src/emscripten_callbacks.cpp
+++ websocket/src/emscripten_callbacks.cpp
@@ -7,21 +7,15 @@ namespace dmWebsocket
 EM_BOOL Emscripten_WebSocketOnOpen(int eventType, const EmscriptenWebSocketOpenEvent *websocketEvent, void *userData) {
     DebugLog(1, "WebSocket OnOpen");
     WebsocketConnection* conn = (WebsocketConnection*)userData;
-    if (IsConnectionValid(conn))
-    {
-        SetState(conn, STATE_CONNECTED);
-        HandleCallback(conn, EVENT_CONNECTED, 0, 0);
-    }
+    SetState(conn, STATE_CONNECTED);
+    HandleCallback(conn, EVENT_CONNECTED, 0, 0);
     return EM_TRUE;
 }
 EM_BOOL Emscripten_WebSocketOnError(int eventType, const EmscriptenWebSocketErrorEvent *websocketEvent, void *userData) {
     DebugLog(1, "WebSocket OnError");
     WebsocketConnection* conn = (WebsocketConnection*)userData;
-    if ( IsConnectionValid(conn))
-    {
-        conn->m_Status = RESULT_ERROR;
-        SetState(conn, STATE_DISCONNECTED);
-    }
+    conn->m_Status = RESULT_ERROR;
+    SetState(conn, STATE_DISCONNECTED);
     return EM_TRUE;
 }
 EM_BOOL Emscripten_WebSocketOnClose(int eventType, const EmscriptenWebSocketCloseEvent *websocketEvent, void *userData) {
@@ -36,15 +30,12 @@ EM_BOOL Emscripten_WebSocketOnClose(int eventType, const EmscriptenWebSocketClos
 EM_BOOL Emscripten_WebSocketOnMessage(int eventType, const EmscriptenWebSocketMessageEvent *websocketEvent, void *userData) {
     DebugLog(1, "WebSocket OnMessage");
     WebsocketConnection* conn = (WebsocketConnection*)userData;
-    if (IsConnectionValid(conn))
+    int length = websocketEvent->numBytes;
+    if (websocketEvent->isText)
     {
-        int length = websocketEvent->numBytes;
-        if (websocketEvent->isText)
-        {
-            length--;
-        }
-        PushMessage(conn, MESSAGE_TYPE_NORMAL, length, websocketEvent->data);
+        length--;
     }
+    PushMessage(conn, MESSAGE_TYPE_NORMAL, length, websocketEvent->data);
     return EM_TRUE;
 }
 
